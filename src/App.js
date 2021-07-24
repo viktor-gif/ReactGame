0ab15d0b4e23db7dc814d8e220bc5d2e561d81bf
viktor-gif <klim.victor.ua@gmail.com>
2021-07-24 17:25:20 +0300

    2.0 (lesson 22): Hook useState, useState in object

--- src/App.js
+++ src/App.js
@@ -1,15 +1,74 @@
 import "./App.css";
 import React, { useState } from "react";
 
+// function App() {
+//   console.log("APP-RENDERING");
+
+//   let [playerCounter1, setPlayerCounter1] = useState(10);
+//   let [playerCounter2, setPlayerCounter2] = useState(10);
+
+//   const decrement = () => {
+//     setPlayerCounter1((actual) => actual - 1);
+//     setPlayerCounter2((actual) => actual - 1);
+//   };
+
+//   return (
+//     <div className="App">
+//       <div className="gameWrapper">
+//         <div className="firstGamer">
+//           <div className="gamerName">Иван Иванович</div>
+//           <div className="gamerProgress">{playerCounter1}</div>
+//           <div className="incrementButton">
+//             <button
+//               onClick={() => {
+//                 setPlayerCounter1((actual) => actual + 1);
+//               }}
+//             >
+//               +
+//             </button>
+//           </div>
+//         </div>
+//         <div className="secondGamer">
+//           <div className="gamerName">Пётр петров</div>
+//           <div className="gamerProgress">{playerCounter2}</div>
+//           <div className="incrementButton">
+//             <button
+//               onClick={() => {
+//                 setPlayerCounter2((actual) => actual + 1);
+//               }}
+//             >
+//               +
+//             </button>
+//           </div>
+//         </div>
+//         <div className="decrementButton">
+//           <button onClick={decrement}>-</button>
+//           <button
+//             onClick={() => {
+//               setPlayerCounter1(10);
+//               setPlayerCounter2(10);
+//             }}
+//           >
+//             reset
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 function App() {
   console.log("APP-RENDERING");
 
-  let [playerCounter1, setPlayerCounter1] = useState(10);
-  let [playerCounter2, setPlayerCounter2] = useState(10);
+  let [playerCounters, setPlayerCounters] = useState({
+    c1: 10,
+    c2: 10,
+  });
 
   const decrement = () => {
-    setPlayerCounter1((actual) => actual - 1);
-    setPlayerCounter2((actual) => actual - 1);
+    setPlayerCounters((actual) => {
+      return { ...actual, c1: actual.c1 - 1, c2: actual.c2 - 1 };
+    });
   };
 
   return (
@@ -17,11 +76,13 @@ function App() {
       <div className="gameWrapper">
         <div className="firstGamer">
           <div className="gamerName">Иван Иванович</div>
-          <div className="gamerProgress">{playerCounter1}</div>
+          <div className="gamerProgress">{playerCounters.c1}</div>
           <div className="incrementButton">
             <button
               onClick={() => {
-                setPlayerCounter1((actual) => actual + 1);
+                setPlayerCounters((actual) => {
+                  return { ...actual, c1: actual.c1 + 1 };
+                });
               }}
             >
               +
@@ -30,11 +91,13 @@ function App() {
         </div>
         <div className="secondGamer">
           <div className="gamerName">Пётр петров</div>
-          <div className="gamerProgress">{playerCounter2}</div>
+          <div className="gamerProgress">{playerCounters.c2}</div>
           <div className="incrementButton">
             <button
               onClick={() => {
-                setPlayerCounter2((actual) => actual + 1);
+                setPlayerCounters((actual) => {
+                  return { ...actual, c2: actual.c2 + 1 };
+                });
               }}
             >
               +
@@ -45,8 +108,9 @@ function App() {
           <button onClick={decrement}>-</button>
           <button
             onClick={() => {
-              setPlayerCounter1(10);
-              setPlayerCounter2(10);
+              setPlayerCounters((actual) => {
+                return { ...actual, c1: 10, c2: 10 };
+              });
             }}
           >
             reset
